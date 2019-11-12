f4878140753ea68800b180f5d08885dad8d815c7
yami03 <seula00027@gmail.com>
2019-11-13 02:26:45 +0900

    Add signin screen css

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,8 +1,64 @@
 import React, { Component } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import styled from "styled-components";
+import * as Font from "expo-font";
 import { logInFacebook, getToken } from "../api";
 
+const Container = styled.View`
+  background: #000;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CatImage = styled.Image`
+  background: #000;
+  width: 100%;
+  height: 40%;
+  transform: rotate(340deg);
+  margin: -15% 0 5% -15%;
+`;
+
+const Title = styled.Text`
+  color: #fff;
+  font-size: 40px;
+  font-weight: bold;
+  padding: 0 40px;
+`;
+
+const Background = styled.ImageBackground`
+  width: 220px;
+  height: 64px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LoginButton = styled.TouchableOpacity`
+  margin-top: 50px;
+`;
+
+const LoginText = styled.Text`
+  color: #dbfff8;
+  font-size: 18px;
+  font-weight: bold;
+  background: black;
+`;
+
 export default class SignInScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontLoaded: false
+    };
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      wallpoet: require("../../assets/fonts/Wallpoet-Regular.ttf")
+    });
+
+    this.setState({ fontLoaded: true });
+  }
+
   async logIn() {
     try {
       const facebookResponse = await logInFacebook();
@@ -29,11 +85,20 @@ export default class SignInScreen extends Component {
 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <TouchableOpacity onPress={() => this.logIn()}>
-          <Text>Login with Facebook</Text>
-        </TouchableOpacity>
-      </View>
+      <Container>
+        <CatImage source={require("../../assets/cat02.gif")} />
+        {this.state.fontLoaded && (
+          <Title style={{ fontFamily: "wallpoet" }}>Starry Night</Title>
+        )}
+        <LoginButton onPress={() => this.logIn()}>
+          <Background
+            source={require("../../assets/bg-heart2.png")}
+            resizeMode="contain"
+          >
+            <LoginText>Facebook Login</LoginText>
+          </Background>
+        </LoginButton>
+      </Container>
     );
   }
 }
